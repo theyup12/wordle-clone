c55c0230942ff29bc57eeb2fc688703a8e9b1c23
theyup12 <dongyicao1234@gmail.com>
2022-04-21 23:01:41 -0700

    sharding database

--- api/shard.py
+++ api/shard.py
@@ -5,17 +5,17 @@ DATABASE = './var/stats.db'
 DATABASE_s1 = './var/stats_s1.db'
 DATABASE_s2 = './var/stats_s2.db'
 DATABASE_s3 = './var/stats_s3.db'
+DATABASE_s4 = './var/user.db'
 SCHEMA = './share/shard.sql'
 
+record_s1 = []
+record_s2 = []
+record_s3 = []
 with contextlib.closing(sqlite3.connect(DATABASE)) as db:
-   db.execute("ATTACH DATABASE ? AS stats_s1", [DATABASE_s1])
-   db.execute("DROP TABLE IF EXISTS games;")
-   db.execute("""
-       CREATE TABLE stats_s1.games(
-       game_id INTEGER NOT NULL PRIMARY KEY,
-       finished DATE DEFAULT CURRENT_TIMESTAMP,
-       guesses INTEGER,
-       won BOOLEAN
-       )""")
-   db.execute("INSERT INTO stats_s1.games SELECT game_id, finished, guesses, won FROM games;")
-   db.commit()
+    for row in db.execute("SELECT * FROM games LIMIT 10 "):
+        if row[0] % 3 == 0:
+            record_s1.append(row)
+        elif row[0] % 3 == 1:
+            record_s2.append(row)
+        else:
+            record_s3.append(row)
